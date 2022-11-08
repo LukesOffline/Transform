8593abc68b1a21464dfe323f66c8de773c5c9e14
Luke Harrison <lukeharr2005@gmail.com>
2022-11-08 15:52:01 +0000

    Added 3D support

---
+++ src/Transform/Acceleration3D.cs
@@ -0,0 +1,33 @@
+﻿namespace Transform
+{
+    using System;
+    using Microsoft.Xna.Framework;
+
+    public class Acceleration3D
+    {
+        public Acceleration3D(Velocity3D velocity)
+        {
+            this.Velocity = velocity ?? throw new ArgumentException(nameof(velocity));
+        }
+
+        public Transform3D Transform => this.Velocity.Transform;
+
+        public Velocity3D Velocity { get; }
+
+        public Vector3 Position { get; set; }
+
+        public Vector3 Scale { get; set; }
+
+        public float Rotation { get; set; }
+
+        public void Update(GameTime time)
+        {
+            var delta = (float)time.ElapsedGameTime.TotalSeconds;
+            this.Velocity.Position += this.Position * delta;
+            this.Velocity.Scale += this.Scale * delta;
+            this.Velocity.Rotation += this.Rotation * delta;
+
+            this.Velocity.Update(time);
+        }
+    }
+}
---
+++ src/Transform/Velocity3D.cs
@@ -0,0 +1,29 @@
+﻿namespace Transform
+{
+    using System;
+    using Microsoft.Xna.Framework;
+
+    public class Velocity3D
+    {
+        public Velocity3D(Transform3D transform)
+        {
+            this.Transform = transform ?? throw new ArgumentException(nameof(transform));
+        }
+
+        public Transform3D Transform { get; }
+
+        public Vector3 Position { get; set; }
+
+        public Vector3 Scale { get; set; }
+        
+        public float Rotation { get; set; }
+
+        public void Update(GameTime time)
+        {
+            var delta = (float)time.ElapsedGameTime.TotalSeconds;
+            this.Transform.Position += this.Position * delta;
+            this.Transform.Scale += this.Scale * delta;
+            this.Transform.Rotation += this.Rotation * delta;
+        }
+    }
+}
